7453be6043eea2eea0fd56af03b08f01333d26cd
Ryan99alero <ryan@randgraphics.com>
2024-06-23 21:49:46 -0500

    Update

--- php/table.Employees.php
+++ php/table.Employees.php
@@ -1,9 +1,6 @@
 <?php
 
-/*
- * Editor server script for DB table Employees
- * Created by http://editor.datatables.net/generator
- */
+require '../assets/config/db.php';
 
 // DataTables PHP library and database connection
 include( "lib/DataTables.php" );
@@ -21,30 +18,9 @@ use
 
 // The following statement can be removed after the first run (i.e. the database
 // table has been created). It is a good idea to do this to help improve
-// performance.
-$db->sql( "CREATE TABLE IF NOT EXISTS `Employees` (
-	`employee_id` int(10) NOT NULL auto_increment,
-	`first_name` varchar(255),
-	`last_name` varchar(255),
-	`email` varchar(255),
-	`department_id` numeric(9,2),
-	`manager_id` numeric(9,2),
-	`role_id` numeric(9,2),
-	`rfid_card_id` numeric(9,2),
-	`external_id` numeric(9,2),
-	`adp_employee_id` numeric(9,2),
-	`employment_type` varchar(255),
-	`temp_agency_id` numeric(9,2),
-	`active` varchar(255),
-	`created_at` datetime,
-	`created_by` varchar(255),
-	`updated_at` datetime,
-	`updated_by` varchar(255),
-	PRIMARY KEY( `employee_id` )
-);" );
 
 // Build our Editor instance and process the data coming from _POST
-Editor::inst( $db, 'Employees', 'employee_id' )
+Editor::inst( $pdo, 'Employees', 'employee_id' )
 	->fields(
 		Field::inst( 'first_name' ),
 		Field::inst( 'last_name' ),
